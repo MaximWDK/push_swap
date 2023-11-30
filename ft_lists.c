9ccef3304b26acb7cce23cbc2ee829dfc667c5a6
MaximWDK <maxim.leonet@gmail.com>
2023-11-30 14:45:36 +0100

    Maneged last leak

--- ft_lists.c
+++ ft_lists.c
@@ -6,7 +6,7 @@
 /*   By: mleonet <mleonet@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 15:50:20 by mleonet           #+#    #+#             */
-/*   Updated: 2023/11/13 15:33:41 by mleonet          ###   ########.fr       */
+/*   Updated: 2023/11/30 14:45:08 by mleonet          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,13 +28,16 @@ int	ft_lstsize(t_list *lst)
 t_list	*ft_lstnew(int content)
 {
 	t_list	*new;
+	t_list	*res;
 
 	new = malloc(sizeof(t_list));
 	if (!new)
 		return (NULL);
 	new->content = content;
 	new->next = NULL;
-	return (new);
+	res = new;
+	ft_lstfree(new);
+	return (res);
 }
 
 void	ft_lstfree(t_list *lst)
